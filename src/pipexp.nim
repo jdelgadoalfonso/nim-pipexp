84fcbe586fdbce1da0c108d214edbb826bf850c1
emanresu3 <emanresu3@noreply.codeberg.org>
2022-11-29 11:12:24 +0000

    Add multiple placeholder proc to pipe macro, previous tests pass again

--- src/pipexp.nim
+++ src/pipexp.nim
@@ -2,13 +2,7 @@ import
   std/macros
 
 
-proc underscorePos(n: NimNode): int =
-  for i in 1 ..< n.len:
-    if n[i].eqIdent("_"): return i
-  return 0
-
-
-proc underscorePos2(n: NimNode): seq[int] =
+proc underscorePos(n: NimNode): seq[int] =
   for i in 1 ..< n.len:
     if n[i].eqIdent("_"):
       result.add(i)
@@ -19,7 +13,7 @@ macro `|`*(arg, fn: untyped): untyped =
   of nnkIdent:
     result = newCall(fn, arg)
   of nnkCall, nnkCommand:
-    var u: seq[int] = underscorePos2(fn)
+    var u: seq[int] = underscorePos(fn)
     result = newNimNode(nnkCall)
       .add(fn[0])
     if fn.len == 1:
@@ -50,9 +44,16 @@ proc underscoredCall(fn, arg0: NimNode): NimNode =
       arg = arg0
     result = newNimNode(nnkCall)
       .add(fn[0])
-    for i in 1..u-1: result.add fn[i]
-    result.add(arg)
-    for i in u+1..fn.len-1: result.add fn[i]
+    if u.len == 0:
+      result.add arg
+      for i in 1..fn.len-1:
+        result.add fn[i]
+    else:
+      for i in 1..fn.len-1:
+        if i in u:
+          result.add arg
+        else:
+          result.add fn[i]
   of nnkStmtList, nnkStmtListExpr:
     result = arg0
     for stmt in fn.children:
