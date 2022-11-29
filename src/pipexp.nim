0f19acb815b8887609d19d0e8982553f6dc9babc
emanresu3 <emanresu3@noreply.codeberg.org>
2022-11-29 11:34:41 +0000

    Add comments

--- src/pipexp.nim
+++ src/pipexp.nim
@@ -1,8 +1,10 @@
 import
   std/macros
 
-
 proc underscorePos(n: NimNode): seq[int] =
+  ## Get the index positions the placeholder arguments
+  ## in the procedure call of `n`
+  ## Empty seq if no placeholder was found
   for i in 1 ..< n.len:
     if n[i].eqIdent("_"):
       result.add(i)
@@ -11,8 +13,10 @@ proc underscorePos(n: NimNode): seq[int] =
 macro `|`*(arg, fn: untyped): untyped =
   case fn.kind:
   of nnkIdent:
+    # When proc is passed without parentheses: arg0 | fn
     result = newCall(fn, arg)
   of nnkCall, nnkCommand:
+    # When proc is passed with parentheses: arg0 | fn(...)
     var u: seq[int] = underscorePos(fn)
     result = newNimNode(nnkCall)
       .add(fn[0])
@@ -37,8 +41,10 @@ macro `|`*(arg, fn: untyped): untyped =
 proc underscoredCall(fn, arg0: NimNode): NimNode =
   case fn.kind:
   of nnkIdent:
+    # When proc is passed without parentheses: arg0 | fn
     result = newCall(fn, arg0)
   of nnkCall, nnkCommand:
+    # When proc is passed with parentheses: arg0 | fn(...)
     let
       u = underscorePos(fn)
       arg = arg0
@@ -55,6 +61,10 @@ proc underscoredCall(fn, arg0: NimNode): NimNode =
         else:
           result.add fn[i]
   of nnkStmtList, nnkStmtListExpr:
+    # When a block of procs is passed as a pipeline:
+    # pipe arg0:
+    #   fn0
+    #   fn1
     result = arg0
     for stmt in fn.children:
       result = underscoredCall(stmt, result)
