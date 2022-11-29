6acf3675541b7ebef866af301887538696eb4792
emanresu3 <emanresu3@noreply.codeberg.org>
2022-11-29 10:54:53 +0000

    Add proc to get multiple placeholder indexes and use it on `|`

--- src/pipexp.nim
+++ src/pipexp.nim
@@ -8,17 +8,33 @@ proc underscorePos(n: NimNode): int =
   return 0
 
 
+proc underscorePos2(n: NimNode): seq[int] =
+  for i in 1 ..< n.len:
+    if n[i].eqIdent("_"):
+      result.add(i)
+
+
 macro `|`*(arg, fn: untyped): untyped =
   case fn.kind:
   of nnkIdent:
     result = newCall(fn, arg)
   of nnkCall, nnkCommand:
-    let u = underscorePos(fn)
+    var u: seq[int] = underscorePos2(fn)
     result = newNimNode(nnkCall)
       .add(fn[0])
-    for i in 1..u-1: result.add fn[i]
-    result.add(arg)
-    for i in u+1..fn.len-1: result.add fn[i]
+    if fn.len == 1:
+      result.add arg
+    else:
+      if u.len == 0:
+        result.add arg
+        for i in 1..fn.len-1:
+          result.add fn[i]
+      else:
+        for i in 1..fn.len-1:
+          if i in u:
+            result.add arg
+          else:
+            result.add fn[i]
   else:
     result = fn
     result.insert(1, arg)
