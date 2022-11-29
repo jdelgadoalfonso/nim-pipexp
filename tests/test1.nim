74a892951468fa08741531d07639e6149b094816
emanresu3 <emanresu3@noreply.codeberg.org>
2022-11-29 14:45:01 +0000

    Add tests for pipelines

--- tests/test1.nim
+++ tests/test1.nim
@@ -3,6 +3,7 @@ import unittest
 import pipexp
 
 proc plus20(arg0: int): int = arg0 + 20
+proc plus30(arg0: int): int = arg0 + 30
 proc plus20Multi(arg1, arg2, arg3: int): int = arg1 + arg2 + arg3 + 20
 
 suite "|":
@@ -16,12 +17,27 @@ suite "|":
     assert plus20(arg0) == arg0 | plus20()
     assert plus20(arg0) == arg0 | plus20(_)
 
+    test "pipelines":
+      assert plus30(plus20(arg0)) == arg0 | plus20 | plus30
+      assert plus20(plus30(arg0)) == arg0 | plus30 | plus20
+      assert plus20(plus20(arg0)) == arg0 | plus20() | plus20
+      assert plus20(plus20(arg0)) == arg0 | plus20 | plus20()
+      assert plus20(plus20(arg0)) == arg0 | plus20() | plus20()
+      assert plus20(plus20(arg0)) == arg0 | plus20(_) | plus20(_)
+
   test "multiple argument procs":
     let arg0 = 10
     assert plus20Multi(arg0,0,0) == arg0 | plus20Multi(0,0)
     assert plus20Multi(arg0,0,0) == arg0 | plus20Multi(_,0,0)
     assert plus20Multi(arg0,arg0,0) == arg0 | plus20Multi(_,_,0)
     assert plus20Multi(arg0,arg0,arg0) == arg0 | plus20Multi(_,_,_)
+
+    test "pipelines":
+      assert plus20Multi(plus20Multi(arg0,0,0),1,1) == arg0 | plus20Multi(0,0) | plus20Multi(1,1)
+      assert plus20Multi(plus20Multi(arg0,0,0),1,1) == arg0 | plus20Multi(_,0,0) | plus20Multi(_,1,1)
+      assert plus20Multi(plus20Multi(arg0,1,2),3,4) == arg0 | plus20Multi(1,2) | plus20Multi(3,4)
+      assert plus20Multi(1,plus20Multi(arg0,0,0),1) == arg0 | plus20Multi(0,0) | plus20Multi(1,_,1)
+      assert plus20Multi(1,1,plus20Multi(arg0,0,0)) == arg0 | plus20Multi(0,0) | plus20Multi(1,1,_)
 
 
 suite "pipe":
