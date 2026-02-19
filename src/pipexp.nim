d13115ee8b5ecf69259c46ff8434b5ec6ec9eb53
Jose A. Delgado <jdelgadoalfonso@gmail.com>
2026-02-19 12:47:46 +0100

    add unpacking placeholder and std/sugar => macro

--- src/pipexp.nim
+++ src/pipexp.nim
@@ -4,50 +4,52 @@ import
 const PLACEHOLDER = "_"
 
 proc placeholderPos(n: NimNode): seq[int] =
-  ## Get the index positions of the placeholder arguments
-  ## in the procedure call of `n`
-  ## Empty seq if no placeholder was found
-  # I first get the positions to know if no placeholder was passed
+  ## Devuelve los índices de los argumentos que contienen el placeholder '_'
   for i in 1 ..< n.len:
-    if n[i].eqIdent(PLACEHOLDER) or
-      (n[i].kind in [nnkBracketExpr, nnkCall] and
-       n[i][0].eqIdent(PLACEHOLDER)):
+    # Caso 1: El argumento es exactamente "_"
+    if n[i].eqIdent(PLACEHOLDER):
       result.add(i)
-
+    # Caso 2: El argumento es una expresión compleja que empieza por "_"
+    # Ej: _[0], _[1], _(10), _.campo
+    elif (n[i].kind in [nnkBracketExpr, nnkCall, nnkDotExpr]) and
+         n[i][0].eqIdent(PLACEHOLDER):
+      result.add(i)
 
 template addArgsAndPlaceholders(phIndices: seq[int], arg, fn: NimNode): untyped =
   if phIndices.len == 0:
+    # Si no hay placeholder explícito, añade 'arg' al principio
     result.add arg
     for i in 1..fn.len-1:
       result.add fn[i]
   else:
+    # Si hay placeholders, sustitúyelos
     for i in 1..fn.len-1:
       if i in phIndices:
         if fn[i].eqIdent(PLACEHOLDER):
-          # Single _ argument
+          # Sustitución directa: arg |> f(_, 1) -> f(arg, 1)
           result.add arg
-        elif (fn[i].kind in [nnkBracketExpr,nnkCall]) and
+        elif (fn[i].kind in [nnkBracketExpr, nnkCall, nnkDotExpr]) and
           fn[i][0].eqIdent(PLACEHOLDER):
-          # _ with indexing/slicing or
-          # with calling parentheses
+          # Sustitución interna: 
+          # arg |> f(_[0]) -> f(arg[0])
+          # arg |> f(_.name) -> f(arg.name)
           fn[i][0] = arg
           result.add fn[i]
       else:
         result.add fn[i]
 
+proc isSugarArrow(n: NimNode): bool =
+  ## Detecta si es una flecha de std/sugar (=>)
+  n.kind == nnkInfix and n[0].eqIdent("=>")
 
-macro `|`*(arg, fn: untyped): untyped =
-  ## Pipe operator. Calls the procedure operand on the right
-  ## with the operand on the left, broadcasting it as
-  ## the first argument or on the placeholder "_" arguments
-
+macro `|>`*(arg, fn: untyped): untyped =
   case fn.kind:
   of nnkIdent:
-    # When proc is passed without parentheses: arg0 | fn
+    # arg |> f
     result = newCall(fn, arg)
 
   of nnkCall, nnkCommand:
-    # When proc is passed with arguments: arg0 | fn(...)
+    # arg |> f(...)
     var u: seq[int] = placeholderPos(fn)
     result = newNimNode(nnkCall)
       .add(fn[0])
@@ -57,10 +59,28 @@ macro `|`*(arg, fn: untyped): untyped =
       addArgsAndPlaceholders(u, arg, fn)
 
   of nnkPar, nnkCurly:
-    if fn[0].kind == nnkLambda:
+    # arg |> (x => x+1)
+    if fn[0].kind == nnkLambda or fn[0].isSugarArrow:
       result = newCall(fn[0], arg)
     else:
-      raise newException(Exception, "expected Lambda expression after '(' or '{'")
+      raise newException(Exception, "expected Lambda or Arrow expression after '(' or '{'")
+  
+  of nnkInfix:
+    # Soporte para sugar directo si el parser lo permite
+    if fn.isSugarArrow:
+      result = newCall(fn, arg)
+    else:
+      result = fn
+      result.insert(1, arg)
+      
+  # Soporte directo para indexación: arg |> _[0]
+  of nnkBracketExpr:
+    if fn[0].eqIdent(PLACEHOLDER):
+      fn[0] = arg
+      result = fn
+    else:
+      # Fallback por si acaso
+      result = newCall(fn, arg)
 
   else:
     result = fn
@@ -83,10 +103,25 @@ proc placeholderCall(fn, arg0: NimNode): NimNode =
     addArgsAndPlaceholders(u, arg, fn)
 
   of nnkPar, nnkCurly:
-    if fn[0].kind == nnkLambda:
+    if fn[0].kind == nnkLambda or fn[0].isSugarArrow:
       result = newCall(fn[0], arg0)
     else:
-      raise newException(Exception, "expected Lambda expression after '(' or '{'")
+      raise newException(Exception, "expected Lambda or Arrow expression after '(' or '{'")
+
+  of nnkInfix:
+    if fn.isSugarArrow:
+      result = newCall(fn, arg0)
+    else:
+      result = newCall(fn, arg0)
+
+  # Soporte para pipe arg: _[0]
+  of nnkBracketExpr:
+    if fn[0].eqIdent(PLACEHOLDER):
+      var newFn = fn
+      newFn[0] = arg0
+      result = newFn
+    else:
+       result = newCall(fn, arg0)
 
   of nnkStmtList, nnkStmtListExpr:
     # When a block of procs is passed as a pipeline:
--- tests/test1.nim
+++ tests/test1.nim
@@ -1,12 +1,16 @@
-import unittest
-
-import pipexp
+import
+  unittest,
+  std/sugar,
+  pipexp
 
 proc plus20(arg0: int): int = arg0 + 20
 proc plus30(arg0: int): int = arg0 + 30
 proc mul3(arg0: int): int = arg0 * 3
 proc plus20Multi(arg1, arg2, arg3: int): int = arg1 + arg2 + arg3 + 20
 proc identityproc(x: proc): proc = x
+proc minus(a, b: int): int = a - b
+proc divInt(a, b: int): int = a div b
+proc formatCoords(x, y, z: int): string = $x & ":" & $y & ":" & $z
 
 import math
 proc power10Sum[T](A: openArray[T]): T =
@@ -15,101 +19,101 @@ proc power10Sum[T](A: openArray[T]): T =
     result +=  A[i] * 10^(len-i-1)
 
 
-suite "|":
+suite "|>":
   let
     arg0 = 10
     A0 = @[1,2,3,4]
 
   test "builtin procs":
     check:
-      arg0 + 20 == arg0 | +20
-      arg0 - 15 == arg0 | - 15
-      arg0 * 2 == arg0 | *2
-      arg0 / 2 == arg0 | /2
-      succ(arg0) == arg0 | succ
-      pred(arg0) == arg0 | pred
+      arg0 + 20 == arg0 |> +20
+      arg0 - 15 == arg0 |> - 15
+      arg0 * 2 == arg0 |> *2
+      arg0 / 2 == arg0 |> /2
+      succ(arg0) == arg0 |> succ
+      pred(arg0) == arg0 |> pred
 
-      true and true == true | and(true)
-      true or false == true | or(false)
-      true xor false == true | xor(false)
+      true and true == true |> and(true)
+      true or false == true |> or(false)
+      true xor false == true |> xor(false)
       # `not` cannot be natively used
-      #not true == true | not()
+      #not true == true |> not()
 
-      (arg0 == arg0) == (arg0 | == arg0)
-      (arg0 != arg0) == (arg0 | != arg0)
-      (arg0 <= arg0+1) == (arg0 | <=(arg0+1))
-      (arg0 >= arg0+1) == (arg0 | >=(arg0+1))
-      (arg0 < arg0+1) == (arg0 | <(arg0+1))
-      (arg0 > arg0+1) == (arg0 | >(arg0+1))
-      arg0 in [arg0,0] == arg0 | in [arg0,0]
+      (arg0 == arg0) == (arg0 |> == arg0)
+      (arg0 != arg0) == (arg0 |> != arg0)
+      (arg0 <= arg0+1) == (arg0 |> <=(arg0+1))
+      (arg0 >= arg0+1) == (arg0 |> >=(arg0+1))
+      (arg0 < arg0+1) == (arg0 |> <(arg0+1))
+      (arg0 > arg0+1) == (arg0 |> >(arg0+1))
+      arg0 in [arg0,0] == arg0 |> in [arg0,0]
 
-      A0[2] == A0 | `[]`(2)
-      A0[1..3] == A0 | `[]`(1..3)
+      A0[2] == A0 |> `[]`(2)
+      A0[1..3] == A0 |> `[]`(1..3)
 
   test "1 argument procs":
     check:
-      plus20(arg0) == arg0 | plus20
-      plus20(arg0) == arg0 | plus20()
-      plus20(arg0) == arg0 | plus20(_)
+      plus20(arg0) == arg0 |> plus20
+      plus20(arg0) == arg0 |> plus20()
+      plus20(arg0) == arg0 |> plus20(_)
 
     test "pipelines 1 argument":
       check:
-        plus30(plus20(arg0)) == arg0 | plus20 | plus30
-        plus20(plus30(arg0)) == arg0 | plus30 | plus20
-        plus20(plus20(arg0)) == arg0 | plus20() | plus20
-        plus20(plus20(arg0)) == arg0 | plus20 | plus20()
-        plus20(plus20(arg0)) == arg0 | plus20() | plus20()
-        plus20(plus20(arg0)) == arg0 | plus20(_) | plus20(_)
+        plus30(plus20(arg0)) == arg0 |> plus20 |> plus30
+        plus20(plus30(arg0)) == arg0 |> plus30 |> plus20
+        plus20(plus20(arg0)) == arg0 |> plus20() |> plus20
+        plus20(plus20(arg0)) == arg0 |> plus20 |> plus20()
+        plus20(plus20(arg0)) == arg0 |> plus20() |> plus20()
+        plus20(plus20(arg0)) == arg0 |> plus20(_) |> plus20(_)
 
   test "multiple argument procs":
     check:
-      plus20Multi(arg0,0,0) == arg0 | plus20Multi(0,0)
-      plus20Multi(arg0,0,0) == arg0 | plus20Multi(_,0,0)
-      plus20Multi(arg0,arg0,0) == arg0 | plus20Multi(_,_,0)
-      plus20Multi(arg0,arg0,arg0) == arg0 | plus20Multi(_,_,_)
+      plus20Multi(arg0,0,0) == arg0 |> plus20Multi(0,0)
+      plus20Multi(arg0,0,0) == arg0 |> plus20Multi(_,0,0)
+      plus20Multi(arg0,arg0,0) == arg0 |> plus20Multi(_,_,0)
+      plus20Multi(arg0,arg0,arg0) == arg0 |> plus20Multi(_,_,_)
 
     test "pipelines multiple argument":
       check:
-       plus20Multi(plus20Multi(arg0,0,0),1,1) == arg0 | plus20Multi(0,0) | plus20Multi(1,1)
-       plus20Multi(plus20Multi(arg0,0,0),1,1) == arg0 | plus20Multi(_,0,0) | plus20Multi(_,1,1)
-       plus20Multi(plus20Multi(arg0,1,2),3,4) == arg0 | plus20Multi(1,2) | plus20Multi(3,4)
-       plus20Multi(1,plus20Multi(arg0,0,0),1) == arg0 | plus20Multi(0,0) | plus20Multi(1,_,1)
-       plus20Multi(1,1,plus20Multi(arg0,0,0)) == arg0 | plus20Multi(0,0) | plus20Multi(1,1,_)
+       plus20Multi(plus20Multi(arg0,0,0),1,1) == arg0 |> plus20Multi(0,0) |> plus20Multi(1,1)
+       plus20Multi(plus20Multi(arg0,0,0),1,1) == arg0 |> plus20Multi(_,0,0) |> plus20Multi(_,1,1)
+       plus20Multi(plus20Multi(arg0,1,2),3,4) == arg0 |> plus20Multi(1,2) |> plus20Multi(3,4)
+       plus20Multi(1,plus20Multi(arg0,0,0),1) == arg0 |> plus20Multi(0,0) |> plus20Multi(1,_,1)
+       plus20Multi(1,1,plus20Multi(arg0,0,0)) == arg0 |> plus20Multi(0,0) |> plus20Multi(1,1,_)
 
   test "placeholder special":
 
     test "placeholder indexing":
       check:
-        plus20(A0[0]) == A0 | plus20(_[0])
-        plus20(A0[^1]) == A0 | plus20(_[^1])
+        plus20(A0[0]) == A0 |> plus20(_[0])
+        plus20(A0[^1]) == A0 |> plus20(_[^1])
 
     test "placeholder slicing":
       check:
-        power10Sum(A0[0..2]) == A0 | power10Sum(_[0..2])
-        power10Sum(A0[0..^1]) == A0 | power10Sum(_[0..^1])
+        power10Sum(A0[0..2]) == A0 |> power10Sum(_[0..2])
+        power10Sum(A0[0..^1]) == A0 |> power10Sum(_[0..^1])
 
     test "placeholder calling":
       check:
-        mul3(plus20(arg0)) == plus20 | mul3(_(arg0))
-        mul3(plus20(arg0)) == plus20 | identityproc | mul3(_(arg0))
-        plus30(plus20(mul3(arg0))) == mul3 | plus20(_(arg0)) | plus30
+        mul3(plus20(arg0)) == plus20 |> mul3(_(arg0))
+        mul3(plus20(arg0)) == plus20 |> identityproc |> mul3(_(arg0))
+        plus30(plus20(mul3(arg0))) == mul3 |> plus20(_(arg0)) |> plus30
 
 
   test "lambdas":
-    check plus20(arg0) == arg0 | (
+    check plus20(arg0) == arg0 |> (
       proc (x: int): int = x + 20
     )
 
     # pipeline with 1 lambda
     test "pipeline lambdas":
-      check plus20(arg0 + 40) == arg0 | (
+      check plus20(arg0 + 40) == arg0 |> (
         proc (x: int): int = x + 40
-      ) | plus20
+      ) |> plus20
 
       # pipeline with multiple lambdas, not next to each other
-      let ret2 = arg0 | (
+      let ret2 = arg0 |> (
         proc (x: int): int = x + 30
-      ) | plus20 | (
+      ) |> plus20 |> (
         proc (x: int): int = x + 30
       )
 
@@ -117,13 +121,13 @@ suite "|":
 
       # pipeline with multiple lambdas, next to each other
       # FIXME: redefinition of ':anonymous'; previous declaration here:
-      let ret3 = arg0 | (
+      let ret3 = arg0 |> (
         proc (x: int): int = x + 30
-      ) | (
+      ) |> (
         proc (x: int): int = x + 30
       )
 
-      check (arg0 + 60) == ret2
+      check (arg0 + 60) == ret3
 
 
 
@@ -217,3 +221,73 @@ suite "pipe":
         mul3(plus20(arg0)) == pipe(plus20, identityproc, mul3(_(arg0)))
         plus30(plus20(mul3(arg0))) == pipe(mul3, plus20(_(arg0)), plus30)
         plus30(plus20(mul3(arg0))) == ret1
+
+
+suite "std/sugar integration":
+  let arg0 = 10
+
+  test "|> with arrow syntax":
+    # IMPORTANTE: Dentro de un 'test' (que es un proc), las flechas
+    # deben tener tipos explícitos: (x: int) => ...
+    # De lo contrario, Nim intenta crear un proc genérico anidado, lo cual es ilegal.
+    check:
+      (arg0 |> ((x: int) => x + 20)) == 30
+      (arg0 |> ((x: int) => x * 2)) == 20
+
+      # Encadenamiento
+      (arg0 |> ((x: int) => x + 5) |> ((y: int) => y * 2)) == 30
+
+  test "pipe macro with arrow syntax":
+    # Mismo caso aquí, necesitamos especificar los tipos de entrada
+    let val1 = pipe arg0:
+      (x: int) => x + 5
+      (x: int) => x * 2
+
+    check val1 == 30
+
+    # Inline pipe
+    # Nota: Aquí usamos paréntesis extra alrededor de la definición de la flecha
+    # para asegurar que el parser no se confunda con las comas del pipe().
+    check pipe(arg0, ((x: int) => x + 5), ((x: int) => x * 2)) == 30
+
+
+suite "Unpacking with _[index]":
+  test "Tuple unpacking":
+    let coords = (10, 5)
+
+    # Resta: 10 - 5
+    check (coords |> minus(_[0], _[1])) == 5
+
+    # Inverso: 5 - 10
+    check (coords |> minus(_[1], _[0])) == -5
+
+    # Operaciones mixtas
+    check (coords |> divInt(_[0], 2)) == 5
+
+  test "Seq/Array unpacking":
+    let list = @[10, 20, 30]
+    let arr = [1, 2, 3]
+
+    # Arrays y secuencias funcionan igual
+    check (list |> minus(_[1], _[0])) == 10 # 20 - 10
+    check (arr |> formatCoords(_[0], _[1], _[2])) == "1:2:3"
+
+  test "Slicing and Negative Indices":
+    let list = @[1, 2, 3, 4]
+
+    # Obtener el último elemento
+    check (list |> _[^1]) == 4
+
+    # Obtener un rango (devuelve seq)
+    check (list |> _[0..1]) == @[1, 2]
+
+  test "Pipeline integration":
+    # (10, 2) -> minus -> 8 -> *2 -> 16
+    check ((10, 2) |> minus(_[0], _[1]) |> ((x: int) => x * 2)) == 16
+
+  test "Pipe macro unpacking":
+    let val = pipe (100, 20):
+      minus(_[0], _[1])  # 100 - 20 = 80
+      divInt(_, 2)       # 80 div 2 = 40
+
+    check val == 40
